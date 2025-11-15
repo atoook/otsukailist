8982d7817f78e21028f1837abfb64c13d16fa344
Kota Kawamoto <atok.reverse.1040@gmail.com>
2025-11-15 14:35:06 +0900

    feat: initialize Spring Boot project with basic structure and configuration files

---
+++ backend/otsukailist/src/test/java/com/atoook/otsukailist/OtsukailistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atoook.otsukailist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtsukailistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
