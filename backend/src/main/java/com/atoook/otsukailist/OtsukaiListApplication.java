24f99a355564038e317355356c62b40226f6007a
Kota Kawamoto <atok.reverse.1040@gmail.com>
2025-11-16 16:37:56 +0900

    refactor: Improve code formatting and add UUID configuration for MySQL

--- backend/src/main/java/com/atoook/otsukailist/OtsukaiListApplication.java
+++ backend/src/main/java/com/atoook/otsukailist/OtsukaiListApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OtsukaiListApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OtsukaiListApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OtsukaiListApplication.class, args);
+    }
 
 }
