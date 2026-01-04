ef2e57c2d1ee997d3665bd67f9f9ba7ed03b9ec7
Kota Kawamoto <atok.reverse.1040@gmail.com>
2026-01-04 10:34:39 +0900

    fix: update span class for improved text clipping behavior in ShareListPage

--- frontend/src/pages/ShareListPage.vue
+++ frontend/src/pages/ShareListPage.vue
@@ -82,9 +82,7 @@ export default {
   <ContentArea layout="center">
     <h1 class="text-2xl font-bold font-sans">共有リンク</h1>
     <div class="mt-6 flex gap-2 px-2 py-1 border border-charcoal-200 bg-charcoal-100 rounded-md w-full">
-      <span
-        class="flex-1 min-w-0 text-charcoal-800 whitespace-nowrap overflow-hidden text-clip overflow-x-scroll scrollbar-hidden"
-      >
+      <span class="flex-1 min-w-0 text-charcoal-800 whitespace-nowrap overflow-hidden overflow-x-auto scrollbar-hidden">
         {{ listURL }}
       </span>
       <MainButton @click="copyUrl" :disabled="!listURL" size="small"> コピー </MainButton>
