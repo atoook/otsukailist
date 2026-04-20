8a2ed1a8e7c5f2e2a8c84df16ff9266c00c1529a
Kota Kawamoto <atok.reverse.1040@gmail.com>
2026-04-20 20:45:23 +0900

    feat: export FEEDBACK_LIST_ID constant for improved accessibility

--- frontend/src/lib/appConstants.ts
+++ frontend/src/lib/appConstants.ts
@@ -1,4 +1,4 @@
-const FEEDBACK_LIST_ID = 'f16857f7-f612-4fa3-9786-aca965e13b83';
+export const FEEDBACK_LIST_ID = 'f16857f7-f612-4fa3-9786-aca965e13b83';
 
 const baseUrl = import.meta.env.VITE_BASE_URL ?? 'https://otsukailist.atok-reverse-1040.workers.dev';
 
